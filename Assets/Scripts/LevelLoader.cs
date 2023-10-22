b89d6ad29508a35b7833826a51466022598b6dcb
fanzhaire <fan990708@gmail.com>
2023-10-22 13:20:48 +1100

    complete 50%

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public void LoadLevel(string sceneName)
+    {
+        
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
